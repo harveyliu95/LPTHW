8f7e6740bd4131f6e8b713347189194aad28339a
harveyliu95 <liutongl5@gmail.com>
2014-11-08 23:18:16 +0800

    Update

---
+++ ex11.py
@@ -0,0 +1,17 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you are %r old, %r tall, and %r heavy." % (
+    age, height, weight)
+
+#study drill
+school = raw_input('Input your school you are currently in ')
+print "You are currently studying in %s" % school
+age = int(raw_input('Input your age here '))
+print "You are now %d years old." % age
+test_string = ("Does a string inside a bracket work?")
+print test_string  #It works without bracket!
