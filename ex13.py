5751a63447a6ba795694fa8bc2b7f27b551cb5ca
harveyliu95 <liutongl5@gmail.com>
2015-01-24 04:57:15 +0800

    update

---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "The first variable is:", first
+print "The second variable is:", second
+print "The third variable is:", third
+
+#questions 5  seems can combine?
+a = int(raw_input())
+print a
